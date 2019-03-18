5f08c456bf89892b2840cdd1c943321c0bd164aa
hoycher <hoycher@abc.com>
2019-03-18 14:28:14 +0700

    Start DataType

---
+++ DataTypeOfVariabla.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร
+
+var myName = "hoycher"
+myName = "Doramon"
+
+//Explicit Constant
+let myFriend = "Nopita"
+//myFriend = "Sunako" Error เพราะ  myFriend เป็น ค่าคงที่  let
+
